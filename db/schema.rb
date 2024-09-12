3cb53226a75e9480efb60ab66f2d92b141dfa197
Sushil Awasthi <sushilawasthi22@gmail.com>
2024-09-12 07:33:55 +0000

    migration db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_11_192310) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_12_034603) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,12 +28,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_11_192310) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "organizations", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "products", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -41,8 +35,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_11_192310) do
     t.string "name"
     t.string "status"
     t.string "unit"
-    t.bigint "tenant_id", null: false
-    t.bigint "user_id", null: false
+    t.bigint "tenant_id"
+    t.bigint "user_id"
     t.index ["tenant_id"], name: "index_products_on_tenant_id"
     t.index ["user_id"], name: "index_products_on_user_id"
   end

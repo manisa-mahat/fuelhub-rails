b675503cb988a81ad2fff14644a6452b8557c0e2
mrbist <sandeshbist36@gmail.com>
2024-09-12 11:00:48 +0000

    Outlet validation with consumer.

--- app/graphql/types/inputs/outlet_input.rb
+++ app/graphql/types/inputs/outlet_input.rb
@@ -1,8 +1,10 @@
 module Types
   module Inputs
     class OutletInput < BaseInputObject
+      argument :consumer_outlet_id, ID, required: false
       argument :name, String, required: true
       argument :address, String, required: true
+      argument :consumer_id, ID, required: true
     end
   end
 end
--- app/models/consumer.rb
+++ app/models/consumer.rb
@@ -1,7 +1,4 @@
 class Consumer < ApplicationRecord
   belongs_to :tenant
-  def consumer_id
-    id
-  end
   has_many :consumer_outlets
 end
